63ded4d397b7bfd78a128e7850d470ba0ab29187
adipshr <adi.pshr@gmail.com>
2024-08-09 13:03:29 +0530

    add .env file and change cors config

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const bookRoute = require("./routes/book.js");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
